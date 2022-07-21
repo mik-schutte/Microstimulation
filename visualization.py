3b9b9e324bc1bf20c5876003a5990350527c8d1b
mik-schutte <mik.schutte1998@gmail.com>
2022-07-22 01:48:42 +0200

    added documentation

--- visualization.py
+++ visualization.py
@@ -13,17 +13,26 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 matplotlib.rcParams.update({'font.size':16, 'font.family':'Times New Roman', 'axes.facecolor':'white'})   
 
 def plot_raster_rt(mouse, save=False):
-    '''docstring'''
-    fig, axs = plt.subplots(1, len(mouse.sessions), figsize=(15, 10))
-    plt.subplots_adjust(wspace=1,)
+    ''' Creates a figure containing rasterplots of the trial response time.
+
+        INPUT:
+            mouse(Mouse_Data): Dataclass with attributes like id, sessions, all_data and concatenated data
+            save(bool): prompts the user for destination folder path
+        OUTPUT:
+            raster_rt_plot(matplotlib.plt): either a plot is shown or saved
+    '''
+    # Set figure basics 
+    fig, axs = plt.subplots(1, len(mouse.sessions), figsize=(15, 10)) # Size plot according to the number of sessions
+    plt.subplots_adjust(wspace=1,) 
     fig.patch.set_facecolor('white')
     fig.suptitle(str(mouse.id), y=1.05)
     
+    # Get and plot data for every session
     for idx, session in enumerate(mouse.sessions):
         colors = []
         rt_full = mouse.all_data[session]['response_t']
-        x = np.arange(0, len(rt_full), 1)
-        rt = [[rt] for rt in rt_full]
+        x = np.arange(0, len(rt_full), 1) # Initiate x-axis for plotting
+        rt = [[rt] for rt in rt_full] # plt.eventplot requires values as list to ascribe different offsets
         intensity = mouse.full_data['intensity']
         intensity = np.sort(list(set(intensity)))
         cmap = matplotlib.cm.get_cmap('summer', len(intensity)).reversed()
@@ -77,6 +86,7 @@ def plot_raster_rt(mouse, save=False):
     cbar = plt.colorbar(cmappable, cax=cax, ticks=[0, 1], orientation='horizontal')
     cbar.ax.set_xticklabels(['Correct trial low intensity', 'Correct trial high intensity'])
     
+    # Prompt user for destination folder path or show the plot
     if save:
         fname = input('What path do you want to save the .jpg to?')
         fig.savefig(fname+mouse.id+'.jpg', bbox_inches='tight')   
